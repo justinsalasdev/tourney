d0e9b1152e99b685838e6c5e3787dff463db280c
Justin Salas <justinsalasdev@gmail.com>
2021-07-22 13:44:01 +0000

    initial structure

---
+++ src/components/App/App.js
@@ -0,0 +1,7 @@
+import m from "./app.module.sass";
+
+function App() {
+  return <div className={`${m.app} ${m.active}`}>hahahha</div>;
+}
+
+export default App;
---
+++ src/components/Test/Test.js
@@ -0,0 +1,5 @@
+import s from "./test.module.sass";
+
+export default function Test() {
+  return <div className={s.app}>Test</div>;
+}
